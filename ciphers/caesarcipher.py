192079fec4ff63929ff96ea22476abd952e26de2
jjeeeeee <jasonjee53@gmail.com>
2023-12-29 00:41:39 -0400

    Cleaned up ciphers

--- ciphers/caesarcipher.py
+++ ciphers/caesarcipher.py
@@ -1,14 +1,18 @@
 def caesar_cipher(text, key):
+    #Making text a list of characters
     text_chars = list(text)
 
+    #Iterating through all characters
     for i in range(len(text_chars)):
         char = text_chars[i]
 
+        #Shifting letters
         if char.isalpha():
             start = ord('A') if char.isupper() else ord('a')
             numChar = ord(char) - start
             numChar = (numChar + key) % 26
             text_chars[i] = chr(numChar + start)
+        #Shifting numbers
         elif char.isnumeric():
             start = ord('0')
             numChar = ord(char) - start
--- ciphers/vigenerecipher.py
+++ ciphers/vigenerecipher.py
@@ -6,40 +6,45 @@ def find_char(key_pair):
     msg_char = key_pair[0]
     key_char = key_pair[1]
 
+    #Finding ASCII value for shifted character
     if msg_char.isupper():
         if key_char.isupper():
-            char = (ord(msg_char) + ord(key_char) - 2 * ord('A')) % 62
+            char = (ord(msg_char) + ord(key_char) - 2 * ord('A'))
         elif key_char.islower():
-            char = (ord(msg_char) + ord(key_char) - ord('A') - ord('a') + 26) % 62
+            char = (ord(msg_char) + ord(key_char) - ord('A') - ord('a') + 26)
         elif key_char.isdigit():
-            char = (ord(msg_char) + ord(key_char) - ord('A') - ord('0') + 52) % 62
+            char = (ord(msg_char) + ord(key_char) - ord('A') - ord('0') + 52)
     elif msg_char.islower():
         if key_char.isupper():
-            char = (ord(msg_char) + ord(key_char) - ord('a') + 26 - ord('A')) % 62
+            char = (ord(msg_char) + ord(key_char) - ord('a') + 26 - ord('A'))
         elif key_char.islower():
-            char = (ord(msg_char) + ord(key_char) - 2 * ord('a') + 52) % 62
+            char = (ord(msg_char) + ord(key_char) - 2 * ord('a') + 52)
         elif key_char.isdigit():
-            char = (ord(msg_char) + ord(key_char) - ord('a') + 26 - ord('0') + 52) % 62
+            char = (ord(msg_char) + ord(key_char) - ord('a') + 26 - ord('0') + 52)
     elif msg_char.isdigit():
         if key_char.isupper():
-            char = (ord(msg_char) + ord(key_char) - ord('0') + 52 - ord('A')) % 62
+            char = (ord(msg_char) + ord(key_char) - ord('0') + 52 - ord('A'))
         elif key_char.islower():
-            char = (ord(msg_char) + ord(key_char) - ord('0') + 52 - ord('a') + 26) % 62
+            char = (ord(msg_char) + ord(key_char) - ord('0') + 52 - ord('a') + 26)
         elif key_char.isdigit():
-            char = (ord(msg_char) + ord(key_char) - 2 * ord('0') + 104) % 62
+            char = (ord(msg_char) + ord(key_char) - 2 * ord('0') + 104)
 
-    return char
+    #Returning character in appropriate range
+    return char % 62
 
 
 def build_vigenere_table():
     table = {}
+
+    #Defining characters in table
     alphabets_upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     alphabets_lower = alphabets_upper.lower()
     digits = "0123456789"
 
+
     for msg_char in alphabets_upper + alphabets_lower + digits:
         for key_char in alphabets_upper + alphabets_lower + digits:
-            pair = (msg_char, key_char)
+            pair = msg_char, key_char
             char = find_char(pair)
 
             if char in range(0, 26):
@@ -48,17 +53,22 @@ def build_vigenere_table():
               table[pair] = chr(char - 26 + ord('a'))
             elif char in range(52, 62):
               table[pair] = chr(char - 52 + ord('0'))
+
     return table
 
 
 def vigenere_cipher(text, key):
+    #Building vigenere table
     vigenere_table = build_vigenere_table()
-    pattern = re.compile(r'[A-Za-z0-9]')
+
     result = ""
     key_length = len(key)
 
+    #Regular expression for supported characters
+    pattern = re.compile(r'[A-Za-z0-9]')
 
     for i, char in enumerate(text):
+        #Repeating key necessary amount of times
         key_char = key[i % key_length]
 
         #Shifting alphanumeric characters
